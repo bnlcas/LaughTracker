dc3fa9edddb725e72477882f80073da71d4d3b20
bnlcas <bnlcas@gmail.com>
2019-12-13 11:30:08 -0800

    adding laugh track sound and playback - completing prototype

--- laugh_track.py
+++ laugh_track.py
@@ -3,15 +3,15 @@ import voice_activity_detector as vad
 import sounddevice as sd
 import soundfile as sf
 
-#laugh_sound, sample_rate = sf.read('laugh.wav', dtype='float32')
+laugh_sound, sample_rate = sf.read('laughter.wav', dtype='float32')
 sample_rate = 44100
 sample_duration = 0.2  # seconds
 voice_detector = vad.VoiceActivityDetector(sample_rate)
 
 def PlayLaughter():
-    #sd.play(laugh_sound, sample_rate)
-    #status = sd.wait()
-    print 'ha\n'
+    sd.play(laugh_sound, sample_rate)
+    status = sd.wait()
+    #print 'ha\n'
 
 while(True):
     sound_recording = sd.rec(int(sample_duration * sample_rate), samplerate=sample_rate, channels=1)
