cb2350f5e3a903c9b7cb1eb56fb9bb893d14673a
Benjamin Lucas <benjaminlucas@benjamins-mbp.gateway.sonic.net>
2020-07-09 16:03:49 -0700

    add multiple laugh clips

--- laugh_track.py
+++ laugh_track.py
@@ -3,13 +3,17 @@ import voice_activity_detector as vad
 import sounddevice as sd
 import soundfile as sf
 
-laugh_sound, sample_rate = sf.read('laughter.wav', dtype='float32')
+laugh_files = ['laughter.wav', 'laughter_1.wav', 'laughter_2.wav', 'laughter_3.wav', 'laughter_4.wav']
+laugh_sounds = []
+for filename in laugh_files:
+    sound, sample_rate = sf.read('/LaughterSounds/' + filename, dtype='float32')
+    laugh_sounds.append(sound)
 sample_rate = 44100
 sample_duration = 0.2  # seconds
 voice_detector = vad.VoiceActivityDetector(sample_rate)
 
 def PlayLaughter():
-    sd.play(laugh_sound, sample_rate)
+    sd.play(laugh_sounds[np.random.randint(len(laugh_sounds))], sample_rate)
     status = sd.wait()
 
 while(True):
